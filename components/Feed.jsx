34b6ce339295b6274f34a592f4af1cd6c6ef4740
Yashwin Sudarshan <yashwin.sudarshan@fdmgroup.com>
2023-05-27 22:49:44 +1000

    Further debugging

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -58,6 +58,7 @@ const Feed = () => {
     const fetchPosts = async () => {
       const response = await fetch("/api/prompt");
       const data = await response.json();
+      console.log(data);
 
       setPosts(data);
     };
